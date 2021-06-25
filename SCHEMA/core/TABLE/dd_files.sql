c68e9157d56d857838b1eb6c17ab0714b78ce106
Александр Краснов <akrasnov87@gmail.com>
2021-06-25 22:23:49 +0300

    Обновление

--- SCHEMA/core/TABLE/dd_files.sql
+++ SCHEMA/core/TABLE/dd_files.sql
@@ -1,12 +1,12 @@
 CREATE TABLE core.dd_files (
 	id uuid DEFAULT public.uuid_generate_v4() NOT NULL,
-	ba_foto bytea NOT NULL,
+	ba_jpg bytea NOT NULL,
 	ba_pdf bytea NOT NULL
 );
 
 ALTER TABLE core.dd_files OWNER TO mobnius;
 
-COMMENT ON COLUMN core.dd_files.ba_foto IS 'Фото ПЦР';
+COMMENT ON COLUMN core.dd_files.ba_jpg IS 'Фото ПЦР';
 COMMENT ON COLUMN core.dd_files.ba_pdf IS 'PDF';
 
 --------------------------------------------------------------------------------
